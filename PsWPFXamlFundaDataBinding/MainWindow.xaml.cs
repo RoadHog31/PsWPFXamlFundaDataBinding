cd9d9856bee884f47b903586dab3b14ff9289fba
Roadhog31 <RoadHog31@users.noreply.github.com>
2019-05-14 20:40:32 +0100

    Add project files.

---
+++ PsWPFXamlFundaDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PsWPFXamlFundaDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person src = new Person { Name = "Steve", Age = 37 };
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = src;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = src.Name + " is " + src.Age;
+            MessageBox.Show(message);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            src.Age += 0.1;
+
+            //ageTextBox.GetBindingExpression(TextBox.TextProperty).UpdateTarget();
+        }
+    }
+}
---
+++ PsWPFXamlFundaDataBinding/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using PsWPFXamlFundaDataBinding.Annotations;
+
+namespace PsWPFXamlFundaDataBinding
+{
+    public class Person : INotifyPropertyChanged
+    {
+
+        private string _nameValue;
+        public string Name
+        {
+            get { return _nameValue; }
+            set { _nameValue = value; }
+        }
+
+        private double ageValue;
+        public double Age
+        {
+            get { return ageValue; }
+            set
+            {
+                if (value != ageValue)
+                {
+                    ageValue = value;
+                   OnPropertyChanged("Age"); 
+                }
+                
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
